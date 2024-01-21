924e77ec4c2736a6b0e6691fb6a0b2ae164cf20a
yayar <yayar@JAREKLAPTOP>
2024-01-21 22:52:55 +0300

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JrkWeather
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
